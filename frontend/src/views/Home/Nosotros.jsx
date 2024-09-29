b6cf4a9f6c96ef9d19d38dcf5cfe211a9fbe5a5f
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-09-29 15:54:08 -0500

    Creacion de vistas

---
+++ frontend/src/views/Home/Nosotros.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/layout/Header"
+import Footer from "../../components/layout/Footer"
+function Nosotros() {
+  return (
+    <>
+      <Header />
+      Esta es la pagina de Nosotros
+      <Footer /> 
+    </>
+  )
+}
+
+export default Nosotros
