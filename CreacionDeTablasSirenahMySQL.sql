fdca2d71078aed94318d72fee8ea6bb9a2c5870a
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-10-19 16:02:15 -0500

    Mod

--- CreacionDeTablasSirenahMySQL.sql
+++ CreacionDeTablasSirenahMySQL.sql
@@ -4,7 +4,7 @@ CREATE TABLE Administrador (
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  contrasena VARCHAR(50) NOT NULL,
+  contrasena VARCHAR(60) NOT NULL,
   telefono VARCHAR(9) NOT NULL,
   estado BIT DEFAULT 1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
