b8e101dcff2a4778048945a85fc6340ca130c1c0
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-11-29 17:56:37 -0500

    Cors Config

--- backend/src/main/java/com/sirenah/backend/config/CorsConfig.java
+++ backend/src/main/java/com/sirenah/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://sirenah-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*"); // Cabeceras permitidas
             }
