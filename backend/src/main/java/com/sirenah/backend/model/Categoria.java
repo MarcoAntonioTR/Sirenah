c0b1f8f096a85b21aa882c77785b14bdb7862a46
MarcoAntonioTR1 <u22208286@utp.edu.pe>
2024-11-29 23:16:26 -0500

    Correccion de Columna Id

--- backend/src/main/java/com/sirenah/backend/model/Categoria.java
+++ backend/src/main/java/com/sirenah/backend/model/Categoria.java
@@ -2,6 +2,8 @@ package com.sirenah.backend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 public class Categoria {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idCategoria;
     @Column(name = "nombre" , length = 100)
     private String Nombre;
--- backend/src/main/java/com/sirenah/backend/model/Direccion.java
+++ backend/src/main/java/com/sirenah/backend/model/Direccion.java
@@ -2,6 +2,8 @@ package com.sirenah.backend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 
 public class Direccion {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idDireccion;
     @Column(name = "descripcion", length = 100)
     private String Descripcion;
