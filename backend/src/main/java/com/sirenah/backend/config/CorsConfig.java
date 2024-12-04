87459348bb598cc96476a23da97ae14092d16898
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-12-03 20:20:39 -0500

    Error 3

--- backend/src/main/java/com/sirenah/backend/config/CorsConfig.java
+++ backend/src/main/java/com/sirenah/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://sirenah-production.up.railway.app")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true)
                         .allowedHeaders("*"); 
