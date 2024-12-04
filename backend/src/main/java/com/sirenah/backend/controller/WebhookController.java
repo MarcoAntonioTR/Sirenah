87a916633f96f565d999db3d942b821496a4cabf
MarcoAntonioTR1 <u22208286@utp.edu.pe>
2024-12-04 17:40:29 -0500

    Error Pago 2

--- backend/src/main/java/com/sirenah/backend/controller/WebhookController.java
+++ backend/src/main/java/com/sirenah/backend/controller/WebhookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/public/webhook")
 public class WebhookController {
     Dotenv dotenv = Dotenv.load();
     private String mercadoPagoToken = dotenv.get("MERCADOPAGO_TOKEN");
